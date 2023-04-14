8183f3fc82ef6b45ee7588863d1747b9c08ca22f
xiaok <wzgbking@gmail.com>
2023-04-15 05:56:51 +0900

    vault

---
+++ contracts/src/Vault.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+contract Kiwiswap is Ownable {
+
+
+    function withdraw() public {
+
+    }
+
+}
