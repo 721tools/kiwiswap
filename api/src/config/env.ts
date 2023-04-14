887bbbdf13aad6702d9e75ea34b125cee4139a5d
veryyoung <veryyoung.me@gmail.com>
2023-04-15 02:59:48 +0900

    Remove module api

--- api/src/config/env.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-
-const path = {
-  development: '.env.development',
-  staging: '.env.staging',
-  production: '.env.production',
-}[process.env.NODE_ENV || 'development'];
-
-dotenv.config({ path });
