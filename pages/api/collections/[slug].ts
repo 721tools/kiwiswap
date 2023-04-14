bde0da3b7bf23b60130cbb441ff891a23227cee9
veryyoung <veryyoung.me@gmail.com>
2023-04-14 21:07:02 +0900

    Add collection api

---
+++ pages/api/collections/[slug].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { slug } = req.query
+  const response = await fetch(`https://testnets-api.opensea.io/api/v1/collection/${slug}?format=json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const json = await response.json();
+  res.status(200).json(json)
+}
