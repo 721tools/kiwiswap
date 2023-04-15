851b3a92884d326bb7f0d8e7656f6bf807cd57c3
xxsue <suki.luux1@gmail.com>
2023-04-15 11:04:29 +0900

    add connect wallet button

--- pages/collections/[id].tsx
+++ pages/collections/[id].tsx
@@ -112,18 +112,18 @@ export default function Collections() {
   };
 
   const connectWallet = async () => {
-    // if (window.ethereum) {
-    //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    //   await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-    //   const signer = provider.getSigner();
-    //   const address = await signer.getAddress();
-    //   setWallet(address);
-    //   console.log(`Connected to MetaMask wallet with address ${address}`);
-    // } else {
-    //   console.log('MetaMask wallet not detected');
-    // }
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setWallet(address);
+      console.log(`Connected to MetaMask wallet with address ${address}`);
+    } else {
+      console.log('MetaMask wallet not detected');
+    }
   }
 
   useEffect(() => {
@@ -145,7 +145,7 @@ export default function Collections() {
           rawData.push({
             key: item.order_hash,
             id: item.protocol_data.parameters.offer[0].identifierOrCriteria,
-            img: item.maker.profile_img_url,
+            img: item.maker_asset_bundle.assets[0].image_url,
             name: `${id} #${item.protocol_data.parameters.offer[0].identifierOrCriteria}`,
             price: etherAmount,
             time: formatTimestamp(item.protocol_data.parameters.startTime)
@@ -161,11 +161,11 @@ export default function Collections() {
 
   return (
     <div className="w-11/12 m-auto">
-      {/* <Button type="primary" size="large"
+      <Button type="primary" size="large"
         onClick={() => connectWallet()}
       >
         {wallet || "ConnectWallet"}
-      </Button> */}
+      </Button>
 
       <Button type="primary" disabled={!select || select.length <= 0} size="large" onClick={() => postCollectionListings(address, select)}>
         BUY
